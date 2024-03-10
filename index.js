3d81bacb2e96eab864bc7f97e45a6d6bf13cbde4
alexgaudineeralexgaudineer <130730219+alexgaudineeralexgaudineer@users.noreply.github.com>
2024-03-09 23:19:25 -0600

    index change

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ let productsGrid = document.getElementById("products-grid");
 // fetch json data
 fetch('./data.json')
   .then((response) => response.json())
-  .then((products) => loadProducts(products))
+  .then((products) => loadProducts(products.dogs))
   .catch(function (err) {
     console.log("ERROR" + err);
   });
 
 // load products grid with all the data in the data.json file
 function loadProducts(data) {
-  let productHTML = ``;
+  let productHTML = '';
 
-  productHTML += createSection(data.dogs.products, "Dog Products");
-  productHTML += createSection(data.dogs.clinics, "Dog Clinics");
-  productHTML += createSection(data.dogs.food, "Dog Food");
+  productHTML += createSection(data.products, "Dog Products");
+  productHTML += createSection(data.clinics, "Dog Clinics");
+  productHTML += createSection(data.food, "Dog Food");
 
   productsGrid.innerHTML = productHTML;
 }
@@ -23,27 +23,35 @@ function loadProducts(data) {
 function createSection(products, categoryName) {
   let sectionHTML = `
     <div class="row mb-4">
-      <h2>${categoryName}</h2>
+      <div class="col">
+        <h3>${categoryName}</h3>
+      </div>
+    </div>
+    <div class="row mb-4">
+      ${generateProductHTML(products)}
+    </div>
   `;
+  return sectionHTML;
+}
 
+// Generate HTML for products
+function generateProductHTML(products) {
+  let productHTML = '';
   for (let i = 0; i < products.length; i++) {
     let product = products[i];
 
-    sectionHTML += `
-      <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-          <div class="mb-1 p-3 d-flex justify-content-center align-items-center" style="height: 200px">
-            <img src="./${product.image}" class="img-fluid" style="max-height: 100%" />
-          </div>
-          <div class="overflow-hidden px-3 mt-2" style="height: 50px">
-            ${product.name}
-          </div>
-          <div class="mb-2 px-3 fw-bold">$${product.priceInCents / 100}</div>
+    productHTML += `<div class="col-4 mb-3"> <!-- col-4 for 3 cards in a row -->
+      <div class="card" style="height: 100%;">
+        <div class="mb-1 p-3 d-flex justify-content-center align-items-center" style="height: 100%;">
+          <img src="./${product.image}" class="img-fluid" style="width: 100%; height: 100%; object-fit: cover;" />
         </div>
+        <div class="overflow-hidden px-3 mt-2" style="height: 50px;">
+          ${product.name}
+        </div>
+        <div class="mb-2 px-3 fw-bold">$${product.priceInCents / 100}</div>
       </div>
-    `;
+    </div>`;
   }
 
-  sectionHTML += `</div>`;
-  return sectionHTML;
+  return productHTML;
 }
